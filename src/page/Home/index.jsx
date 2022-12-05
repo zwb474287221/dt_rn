92e4041a7d8925fb29ac2ae0872476a81562ded0
xiaoheng.lj <xiaoheng.lj@alibaba-inc.com>
2022-12-05 22:29:47 +0800

    feat: 1

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button, AppNavigation} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {getBaseStyles} from '@/styles/base';
 
 export default function Index() {
